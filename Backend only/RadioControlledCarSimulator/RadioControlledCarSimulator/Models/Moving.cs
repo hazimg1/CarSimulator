ddf319c1a3aca4210eb13a4d2d8c2d82fa689ea4
Hazim Abdulkareem <Hazim.Abdulkareem@nexergroup.com>
2024-08-31 16:01:06 +0200

    Add  Directions Enum  and Moving Enum

---
+++ Backend only/RadioControlledCarSimulator/RadioControlledCarSimulator/Models/Moving.cs
@@ -0,0 +1,13 @@
+﻿namespace RadioControlledCarSimulator.Models;
+
+/// <summary>
+/// Represents the possible movements of the car.
+/// </summary>
+public enum Moving
+{
+    None,
+    Left,
+    Right,
+    Forward,
+    Backward
+}
