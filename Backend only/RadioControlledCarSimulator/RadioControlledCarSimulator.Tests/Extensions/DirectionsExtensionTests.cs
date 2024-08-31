6152929fb7ae7ef88f6a9d142406fbe3f4a4ac70
Hazim Abdulkareem <Hazim.Abdulkareem@nexergroup.com>
2024-08-31 18:19:39 +0200

    Add DirectionsExtension class and unit tests
    - Implemented DirectionsExtension class to convert direction characters to Directions enum values.
    - Added unit tests for DirectionsExtension to ensure accurate conversion of direction characters.

---
+++ Backend only/RadioControlledCarSimulator/RadioControlledCarSimulator.Tests/Extensions/DirectionsExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using RadioControlledCarSimulator.Extensions;
+using RadioControlledCarSimulator.Models;
+
+namespace RadioControlledCarSimulator.Tests;
+
+public class DirectionsExtensionTests
+{
+    [Theory]
+    [InlineData('N', Directions.North)]
+    [InlineData('E', Directions.East)]
+    [InlineData('S', Directions.South)]
+    [InlineData('W', Directions.West)]
+    [InlineData('X', Directions.None)]
+    public void ToDirections_ShouldReturnCorrectDirection(char input, Directions expected)
+    {
+        // Act
+        var result = input.ToDirections();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
+
---
+++ Backend only/RadioControlledCarSimulator/RadioControlledCarSimulator/Extensions/DirectionsExtension.cs
@@ -0,0 +1,27 @@
+﻿using RadioControlledCarSimulator.Models;
+using System.Globalization;
+
+namespace RadioControlledCarSimulator.Extensions;
+
+/// <summary>
+/// Provides extension methods for the Directions enum.
+/// </summary>
+public static class DirectionsExtension
+{
+    /// <summary>
+    /// Converts a direction code to a Directions enum value.
+    /// </summary>
+    /// <param name="directionCode">The direction code.</param>
+    /// <returns>The corresponding Directions enum value.</returns>
+    public static Directions ToDirections(this char directionCode)
+    {
+        return char.ToUpper(directionCode, CultureInfo.CurrentCulture) switch
+        {
+            'N' => Directions.North,
+            'E' => Directions.East,
+            'S' => Directions.South,
+            'W' => Directions.West,
+            _ => Directions.None
+        };
+    }
+}
