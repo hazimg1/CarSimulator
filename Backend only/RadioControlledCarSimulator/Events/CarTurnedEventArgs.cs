5fda7b28d0e4195ea4c8a2bab19eb8c40fb3f214
Hazim Abdulkareem <Hazim.Abdulkareem@nexergroup.com>
2024-08-31 16:06:54 +0200

    feat: Add event handling for car movements and turns
    - Introduced CarMovedEventArgs and CarTurnedEventArgs classes for event data.
    - Implemented CarEvent class to handle car movement and turn events.
    - Updated Car class to trigger events on movement and direction changes.

---
+++ Backend only/RadioControlledCarSimulator/Events/CarTurnedEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using RadioControlledCarSimulator.Models;
+
+namespace RadioControlledCarSimulator.Events;
+
+/// <summary>
+/// Provides data for the CarTurned event.
+/// </summary>
+public class CarTurnedEventArgs : EventArgs
+{
+    /// <summary>
+    /// Gets the direction the car is facing.
+    /// </summary>
+    public Directions Direction { get; init; }
+
+    /// <summary>
+    /// Gets the movement direction of the car.
+    /// </summary>
+    public Moving Moving { get; init; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CarTurnedEventArgs"/> class.
+    /// </summary>
+    /// <param name="direction">The direction the car is facing.</param>
+    /// <param name="moving">The movement direction of the car.</param>
+    public CarTurnedEventArgs(Directions direction, Moving moving)
+    {
+        Moving = moving;
+        Direction = direction;
+    }
+}
